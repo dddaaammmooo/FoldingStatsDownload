09f6d59fdc63127e93c7d25129bcd3f538c30b1b
Damian Sloane <dddaaammmooo@hotmail.com>
2017-11-07 09:25:03 +1100

    Add download URL to database configuration

--- laravel/database/seeds/ConfigTableSeeder.php
+++ laravel/database/seeds/ConfigTableSeeder.php
@@ -17,6 +17,10 @@ class ConfigTableSeeder extends Seeder
                 'token' => 'logging.filename',
                 'value' => 'storage/logs/foldingcoin.log',
             ],
+            [
+                'token' => 'download.url',
+                'value' => 'http://fah-web.stanford.edu/daily_user_summary.txt.bz2'
+            ]
         ];
 
         DB::table(Config::getTableName())->insert($config);
