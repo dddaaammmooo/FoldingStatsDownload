15c160143e08057b571b6bc206d3a984a920ba02
Damian Sloane <dddaaammmooo@hotmail.com>
2017-11-07 10:40:46 +1100

    Verbose log download url/timeout

--- laravel/app/Services/Stats/DownloadService/Mock/DownloadService.php
+++ laravel/app/Services/Stats/DownloadService/Mock/DownloadService.php
@@ -3,6 +3,7 @@
 namespace App\Services\Stats\DownloadService\Mock;
 
 use App\Services\ConfigLoaderService\IConfigLoaderService;
+use App\Services\LoggingService\ILoggingService;
 use App\Services\Stats\DownloadService\IDownloadService;
 use App\Services\Stats\DownloadService\Result;
 use Illuminate\Contracts\Filesystem\FileNotFoundException;
@@ -20,9 +21,22 @@ class DownloadService implements IDownloadService
     /** @var IConfigLoaderService */
     private $configLoader;
 
-    public function __construct(IConfigLoaderService $configLoader)
+    /** @var ILoggingService */
+    private $loggingService;
+
+    /**
+     * DownloadService constructor.
+     *
+     * @param IConfigLoaderService $configLoader
+     * @param ILoggingService      $loggingService
+     */
+    public function __construct(
+        IConfigLoaderService $configLoader,
+        ILoggingService $loggingService
+    )
     {
         $this->configLoader = $configLoader;
+        $this->loggingService = $loggingService;
     }
 
     /**
@@ -38,10 +52,16 @@ class DownloadService implements IDownloadService
         // Get the download URL and timeout
 
         $downloadUrl = $this->configLoader->get('download.url');
+
+        $this->loggingService->LogDebug("Stats Download URL: {$downloadUrl}");
+
         $downloadTimeout = $this->configLoader->get('download.timeout');
 
+        $this->loggingService->LogDebug("Stats Download Timeout: {$downloadTimeout} Seconds");
+
         try {
-            // The download URL is actually a local storage patch for the purposes of the mock
+            // The download URL is actually a local storage patch for the purposes of the mock, so just retrieve it
+            // directly from disk
 
             $bz2File = Storage::get($downloadUrl);
 
