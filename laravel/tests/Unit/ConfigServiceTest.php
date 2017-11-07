f09343b35c532c4f51cec2819bed3f476de5ce50
Damian Sloane <dddaaammmooo@hotmail.com>
2017-11-07 11:39:36 +1100

    Fake URL test

--- laravel/tests/Unit/ConfigServiceTest.php
+++ laravel/tests/Unit/ConfigServiceTest.php
@@ -8,8 +8,13 @@ use Tests\TestCase;
 
 class ConfigServiceTest extends TestCase
 {
+    // Known default mock values
+
+    const DOWNLOAD_URL = 'tests/DownloadService/daily_user_summary.txt.bz2';
+    const DOWNLOAD_TIMEOUT = '300';
+
     /**
-     * Test retrieval of download URL
+     * Test retrieval of default download URL
      *
      * @return void
      * @throws ConfigLoaderServiceException
@@ -18,11 +23,11 @@ class ConfigServiceTest extends TestCase
     {
         $configLoader = new ConfigLoaderService();
         $downloadUrl = $configLoader->get('download.url');
-        $this->assertEquals('tests/DownloadService/daily_user_summary.txt.bz2', $downloadUrl);
+        $this->assertEquals(self::DOWNLOAD_URL, $downloadUrl);
     }
 
     /**
-     * Test retrieval of download timeout
+     * Test retrieval of default download timeout
      *
      * @return void
      * @throws ConfigLoaderServiceException
@@ -32,5 +37,34 @@ class ConfigServiceTest extends TestCase
         $configLoader = new ConfigLoaderService();
         $downloadTimeout = $configLoader->get('download.timeout');
         $this->assertEquals('300', $downloadTimeout);
+    }
+
+    /**
+     * Test update and subsequent retrieval of download timeout
+     *
+     * @return void
+     * @throws ConfigLoaderServiceException
+     */
+    public function testSetConfigDownloadUrl()
+    {
+        $configLoader = new ConfigLoaderService();
+
+        // Create a fake URL
+
+        $fakeUrl = 'http://www.fake.com/stats.bz2';
+
+        // Test default value
+
+        $downloadUrl = $configLoader->get('download.url');
+        $this->assertEquals(self::DOWNLOAD_URL, $downloadUrl);
+
+        // Save a fake URL
+
+        $configLoader->set('download.url', $fakeUrl);
+
+        // Read fake URL back
+
+        $downloadUrl = $configLoader->get('download.url');
+        $this->assertEquals($fakeUrl, $downloadUrl);
     }
 }
